e2a803dafd807b7e85849b4e7a21b3b0282ab09f
Indu Chandrasekharan <icanalytics2019@gmail.com>
2019-09-20 20:53:58 -0500

    Adding file to my branch

---
+++ fortune500/app.py
@@ -0,0 +1,83 @@
+import os
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+###########################
+# Database Setup
+###########################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:password@localhost:5432/fortune500_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# from .models import Fortune500, Sector_Industry
+
+class Fortune500(db.Model):
+	__tablename__ = 'fortune500'
+
+	Rank = db.Column(db.Integer, primary_key=True)
+	Title = db.Column(db.String(250))
+	Employees = db.Column(db.Integer)
+	CEO = db.Column(db.String(250))
+	CEO_Title = db.Column(db.String(300))
+	Sector = db.Column(db.String(300))
+	Industry = db.Column(db.String(300))
+	Years_on_Fortune_500_List = db.Column(db.String(5))
+	City = db.Column(db.String(100))
+	State = db.Column(db.String(100))
+	Latitude = db.Column(db.Float)
+	Longitude = db.Column(db.Float)
+	Revenues = db.Column(db.Float)
+	Revenue_Change = db.Column(db.Float)
+	Profits = db.Column(db.Float)
+	Profit_Change = db.Column(db.Float)
+	Assets = db.Column(db.Float)
+	Mkt_Value = db.Column(db.Float)
+	Symbol = db.Column(db.String(10))
+
+class Sector_Industry(db.Model):
+    __tablename__ = 'sector_industry'
+
+    Sector = db.Column(db.String(300), primary_key=True)
+    Industry = db.Column(db.String(300))
+    Revenues = db.Column(db.Float)
+    Profits = db.Column(db.Float)
+    Revenue_Percent = db.Column(db.Float)
+    Profit_Percent = db.Column(db.Float)
+    Profit_Margin = db.Column(db.Float)
+
+@app.route("/")
+def index():
+    """Return the homepage"""
+    return render_template("indu.html")
+
+@app.route("/timeseries", methods = ['POST', 'GET'])
+def timeseries():
+    """Return fortune100 companies list"""
+    results = db.session.query(Fortune500.Rank, Fortune500.Title, Fortune500.Symbol).all()
+
+    # Use Pandas to perform the sql query
+    df = pd.DataFrame(results, columns=['Rank','Title', 'Symbol'])
+                                                                                                                                                                           
+    # stockData = df.to_dict(orient="records")
+    stockData = []
+    for row in results:
+       #stockData.append(f'{row["Rank"]} {row["Title"]} {row["Symbol"]}')
+       stockData.append(f'{list(row)[0]} : {list(row)[1]} : {list(row)[2]}')    
+    return render_template('visualization/time.html', data = stockData)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ fortune500/static/js/time1.js
@@ -0,0 +1,118 @@
+/**
+ * Helper function to select stock data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - Date
+ * index 1 - Open
+ * index 2 - High
+ * index 3 - Low
+ * index 4 - Close
+ * index 5 - Volume
+ */
+var stock = [];
+
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
+
+
+  // Submit Button handler
+function handleSubmit(e) {
+
+    // Prevent the page from 
+    console.log("I am inside handle submit")
+    e.preventDefault();
+    
+    selector = d3.select("#selDataset");
+    // Using jQuery getting all the selected items
+    var text = $('#selDataset option:selected').toArray().map(item => item.text).join();
+    var startDate = d3.select("#startDate").node().value;
+    var endDate = d3.select("#endDate").node().value;
+    console.log(startDate);
+    console.log(endDate);
+    console.log(text);
+
+  // Clearing the all the element data from the dropdown from the selection
+  $('#form').trigger("reset");
+  $("#selDataset option:selected").each(function(){
+    $(this).removeAttr("selected");
+  });
+    var stockList = text.split(',');
+    for (var i=0; i < stockList.length; i++) {
+        stock.push(stockList[i].split(':')[2].trim());
+    }
+    
+    console.log(stock);
+    buildPlot(stock,startDate,endDate);
+
+}
+    // Select the input value from the form
+
+  function buildPlot(stock, startDate, endDate) {
+      //var apiKey = "YOUR KEY HERE";
+      var name =  openingPrices = highPrices = lowPrices = closingPrices = dates = trace = [];
+
+      for (var i=0; i < stock.length; i++) {
+        var url = `https://www.quandl.com/api/v3/datasets/WIKI/${stock[i]}.json?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
+
+           
+      d3.json(url).then(function(data) {
+        console.log(data);
+    
+        // Grab values from the response json object to build the plots
+        name[i] = data.dataset.name;
+        //stock[i] = data.dataset.dataset_code;
+        //var startDate = data.dataset.start_date;
+        //var endDate = data.dataset.end_date;
+        dates[i] = unpack(data.dataset.data, 0);
+        openingPrices[i] = unpack(data.dataset.data, 1);
+        highPrices[i] = unpack(data.dataset.data, 2);
+        lowPrices[i] = unpack(data.dataset.data, 3);
+        closingPrices[i] = unpack(data.dataset.data, 4);
+        //trace[i] = `trace${i}`
+         trace1 = {
+          type: "scatter",
+          mode: "lines",
+          name: name[i],
+          x: dates[i],
+          y: closingPrices[i],
+          line: {
+            //color: "#17BECF"
+          }
+        };
+    
+        // Candlestick Trace
+        var trace2 = {
+          type: "candlestick",
+          x: dates[i],
+          high: highPrices[i],
+          low: lowPrices[i],
+          open: openingPrices[i],
+          close: closingPrices[i]
+        };
+    
+        var data = [trace1, trace2 ];
+    
+        var layout = {
+          title: `${stock[i]} closing prices`,
+          xaxis: {
+            range: [startDate, endDate],
+            type: "date"
+          },
+          yaxis: {
+            autorange: true,
+            type: "linear"
+          }
+        };
+    
+        Plotly.newPlot("time", data, layout);
+      }).catch(function(e) {
+        console.error(e);
+      });
+
+    }
+  }
+  
