31c0a2a10d178fc51e0cce2d63a8bbfbb8a20522
cletusajim <cletusajim@gmail.com>
2019-09-24 20:50:50 -0500

    updated logic.js

--- fortune500/static/js/logic.js
+++ fortune500/static/js/logic.js
@@ -1,64 +1,84 @@
-// Data URL
-let dataurl = '/map/data';
-// Create Light map
-let lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+// Create a map object
+var myMap = L.map("map", {
+   center: [37.09, -95.71], //[15.5994, -28.6731],
+   zoom: 6
+   // layers: [
+   //   lightMap
+   // ]
+ });
+ 
+ // Adding tile layer
+ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-   maxZoom: 15,
-   id: "mapbox.light",
+   maxZoom: 18,
+   id: "mapbox.streets-basic",
    accessToken: API_KEY
-})
-// Create layer group
-let layers = {
-   lightearthquake: new L.layerGroup()
-};
-// Initialize map
-let initmap = L.map('map', {
-   center: [40, -100],
-   zoom: 4,
-   layers: [
-       lightMap,
-       layers.lightearthquake
-   ]
-})
-function plotsize(mag) {return mag * 20000}
-// function to get color based on magnitude
-function getcolor(mag) {
-   return mag >= 5 ? '#F06B6B':
-          mag >= 4 ? '#F0A76B':
-          mag >= 3 ? '#F3BA4D':
-          mag >= 2 ? '#F3DB4D':
-          mag >= 1 ? '#E1F34D':
-          '#B7F34D';
-}
-// Function to draw circles on the map
-function buildMarker(features) {
-   features.forEach(feature=>{
-      console.log("For Each");
-       L.circle([feature.lat, feature.long, {
-           fillColor: getcolor(feature.revenue),
-           fillOpacity: .65,
-           color: "#999999",
-           weight: 1,
-           radius: plotsize(feature.revenue) 
-       }]).bindPopup('<p><b>Company</b>: ' + feature.comp)  //+ '</p><p><b>When</b>:' + new Date(feature.properties.time) + '</p>' + '<p><b>Richter</b>: ' + feature.revenue)
-       .addTo(layers.lightearthquake);
-   });
-}
-
-//get data and apply functions created above
-d3.json(dataurl).then(function(data){
-   console.log("JSON returned");
-   buildMarker(data)
-   let legend = L.control({position: 'bottomright'});
-   legend.onAdd = function() {
-       let div = L.DomUtil.create('div', 'legend');
-       let labels = [' 0 - 1 ', ' 1 - 2 ', ' 2 - 3 ', ' 3 - 4 ', ' 4 - 5 ', ' 5 + ']
-       let colors = ['#B7F34D', '#E1F34D', '#F3DB4D', '#F3BA4D', '#F0A76B', '#F06B6B']
-       for (let i = 0; i < colors.length; i++){
-           div.innerHTML +=
-               '<div><li id = "legend" style="background-color:' + colors[i] + '">&nbsp</li>' + labels[i] + '</div>';
-       }
-       return div;
+ }).addTo(myMap);
+ 
+ L.layers = {
+   lightMap
+ }
+ // Adjust the radius based on revenue
+ function markerSize(rev) {
+   return rev / 5;
+ }
+ 
+ function visualize(rank) {
+   if (rank < 50) {
+     color = "teal"
    }
-   legend.addTo(initmap);
-});
+   else if (rank < 100) {
+     color = "peach"
+   }
+   else if (rank < 150) {
+     color = "yellow"
+   }
+   else if (rank < 200) {
+     color = "limegreen"
+   }
+   else if (rank < 250) {
+     color = "red"
+   }
+   else if (rank < 300) {
+     color = "mint"
+   }
+   else if (rank < 350) {
+     color = "purple"
+   }
+   else if (rank < 400) {
+     color = "orange"
+   }
+   else if (rank < 450) {
+     color = "burgundy"
+   }
+   else if (rank < 500) {
+     color = "black"
+   }
+ 
+   return color
+ }
+ //path for the  data
+ var path = "/map/data"
+   
+ d3.json(path).then(function(fortune500) {
+ //    d3.json(url).then(function(response) {
+   console.log(fortune500)
+   
+     fortune500.forEach(company => {
+       console.log(company)
+   //    for (var i = 0; i < companies.length)
+       L.circle([company.lat, company.long], {
+         fillOpacity: 0.75,
+         color: "emerald",
+         fillColor: visualize(company.rank),
+         radius: markerSize(company.revenue)      
+       })
+       .bindPopup("<h1>" + company.comp + "</h1> <hr> <h3>Rank: " + company.rank + "</h3>" + "<h3>Revenue: " 
+       + company.revenue + "</h3>" + "<h3>Employee Count: " + company.emp_cnt)
+       .addTo()
+       // .addTo(myMap);  
+     })
+   // }
+ });
+ //})
+ 
